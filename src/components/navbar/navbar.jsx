539ecc856cf2de56c7c647fe7e33952facbfd287
Leo-Tena GASSAMA <leotena.gassama@acxess-projects.info>
2024-04-03 13:04:11 +0200

    First Code

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+// const Navbar = () => {
+//     return (
+//         // <nav>
+//         //     <img src={src} alt={alt} /> 
+//         // </nav>
+
+//         // <ul>
+            
+//         // </ul>
+//     )
+// }
+export default Navbar;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+
+const Home = () => {
+
+    return (
+        <main>
+            <h1>Accueil</h1>
+        </main>
+    )
+}
+
+export default Home;
